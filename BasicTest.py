514846bbfd7657244109d43aad838ee633551c6b
Haoyuan <wuxidavid@hotmail.com>
2015-01-25 13:19:33 -0500

    Added two basic classes on storing page information (and related comparison) plus tests on basic webpage content

---
+++ BasicTest.py
@@ -0,0 +1,29 @@
+'''
+BasicTest.py
+Some simple tests on the website's status and search for 
+wanted keywords with Requests
+'''
+
+import requests
+
+class BasicTest():
+    #Initialize a testcase with the given url
+    def __init__(self, url):
+        self.page = requests.get(url)
+
+    def teststatus(self):
+        if 3 < self.page.status_code//100 < 6:
+            return False
+        else: return True
+    
+    def testkeyword(self, keyword):
+        if keyword in self.page.text:
+            return True
+        else: return False
+
+
+if __name__ == '__main__':
+    t = BasicTest('http://localhost:8080/version1/menu.html')
+    print(t.teststatus())
+    print(t.testkeyword('Not Found'))
+
---
+++ PageContent.py
@@ -0,0 +1,38 @@
+'''
+PageContent.py
+Storing the information of a webpage
+Given that the content is valid (no 
+'''
+import requests, functools
+
+@functools.total_ordering
+class Page:
+    def __init__(self, url, content = None):
+        if isinstance(url, str):
+            self.url = url.lower()
+        else: raise NotImplementedError
+
+        if content == None:
+            self.content = requests.get(self.url).text
+        elif isinstance(content, str):
+            self.content = content
+        else: raise NotImplementedError
+
+    def _is_valid_operand(self, other):
+        return hasattr(other, 'url') and hasattr(other, 'content')
+
+    def __lt__(self, other):
+        if not self._is_valid_operand(other):
+            raise NotImplementedError
+        
+        return (self.url, self.content) < (other.url, other.content)
+
+    def __eq__(self, other):
+        if not self._is_valid_operand(other):
+            raise NotImplementedError
+
+        return (self.url, self.content) < (other.url, other.content)
+
+    def __hash__(self):
+        return hash(self.url, self.content)
+## END OF CLASS
